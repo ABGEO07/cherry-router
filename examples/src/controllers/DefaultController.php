186bea1e822ed88fb5423f391cf350746a2bc6c5
ABGEO <takalandzet@gmail.com>
2019-03-07 00:27:15 +0400

    Add base functionality

---
+++ examples/src/controllers/DefaultController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Cherry;
+
+class DefaultController
+{
+    public function index()
+    {
+        echo 'Hello, World!';
+    }
+}
--- examples/test.php
+++ examples/test.php
@@ -5,4 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Cherry\Router;
 
-$router = new Router();
+define('ROUTES_FILE', __DIR__ . '/src/config/routes.json');
+define('CONTROLLERS_PATH', __DIR__ . '/src/controllers');
+
+$router = new Router(ROUTES_FILE, CONTROLLERS_PATH);
