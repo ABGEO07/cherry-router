cb18f43398d8b36646c025a586d8960974b9b5c6
ABGEO <takalandzet@gmail.com>
2019-03-07 18:48:45 +0400

    Update README

--- examples/test.php
+++ examples/test.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 define('__ROOT__', __DIR__);
 define('CONFIG_FILE', __DIR__ . '/config/config.json');
 
+//TODO: Create class for defining application config parameters
 $config = file_get_contents(CONFIG_FILE)
     or die("Unable to open config file!");
 
