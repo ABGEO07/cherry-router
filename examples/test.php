05adb868a170503b29cf19c979e5f3bb35296cac
ABGEO <takalandzet@gmail.com>
2019-03-07 17:46:23 +0400

    Add routes caching feature

--- examples/test.php
+++ examples/test.php
@@ -7,7 +7,7 @@ define('__ROOT__', __DIR__);
 define('CONFIG_FILE', __DIR__ . '/config/config.json');
 
 $config = file_get_contents(CONFIG_FILE)
-    or die("Unable to open routes file!");
+    or die("Unable to open config file!");
 
 $config = json_decode($config, 1);
 
